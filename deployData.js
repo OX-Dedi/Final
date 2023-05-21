f1d4df2ab0af4af2e0d8d16cbeac45ba86a1bf31
Dedi <dedir8361@gmail.com>
2023-05-21 21:51:58 +0700

    first commit

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Dedi Rosadi'; // TODO: replace this
+
+const studentId = 'FE5701086'; // TODO: replace this
+
+const NetlifyDeployUrl = 'MASIH BELUM DIBUAT DAN DI ISI'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
